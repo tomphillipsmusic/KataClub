c0e464326769767af51dd00181f815cd1eee4968
Tom Phillips <tomphillipsmusic@gmail.com>
2022-01-13 14:26:15 -0500

    Initial Commit

---
+++ KataClub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KataClub
+//
+//  Created by Tom Phillips on 1/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KataClub/KataClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  KataClubApp.swift
+//  KataClub
+//
+//  Created by Tom Phillips on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct KataClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
