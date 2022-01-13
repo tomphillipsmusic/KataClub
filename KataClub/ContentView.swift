d3b823ed4b9b53aae2e0c65cf9d7204bcdcdbd5a
Tom Phillips <tomphillipsmusic@gmail.com>
2022-01-13 14:49:10 -0500

    Added list view

--- KataClub/ContentView.swift
+++ KataClub/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                NavigationLink(destination: Text("Pyramid of 1s")) {
+                    Text("Pyramid of 1s")
+                }
+            }
+            .navigationTitle("Kata Club")
+        }
     }
 }
 
