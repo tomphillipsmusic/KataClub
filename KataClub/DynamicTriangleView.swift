56660b2a9612a54dde018b4bb687527acdae8fe2
Tom Phillips <tomphillipsmusic@gmail.com>
2022-01-14 15:09:45 -0500

    Added unit tests for the dynamic triangle function and did some minor cleanup.

---
+++ KataClub/DynamicTriangleView.swift
@@ -0,0 +1,76 @@
+//
+//  PyramidOfOnes.swift
+//  KataClub
+//
+//  Created by Tyler Lawrence on 1/13/22.
+//
+
+import SwiftUI
+
+struct DynamicTriangleView: View {
+    
+    let exampleResult: String = """
+    1
+    11
+    111
+    11
+    1
+    """
+    
+    let invalidInputText = "Invalid Input"
+    
+    @State private var height: String = ""
+    @State private var result: String = ""
+    
+    var body: some View {
+        ScrollView {
+            VStack{
+                Text("Dynamic Triangle")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("Make a function that creates a triangle of '1's. The first row should have one '1' the second has two '1's until the given number (n). After printing n 1's start to go down until you get to one '1'. This should be one string...not a VStack 😀. If a user enters text or a number lower than 1, return the invalid input text instead.")
+                    .padding()
+                Text("Example: n = 3")
+                Text(exampleResult)
+                Divider()
+                TextField("Height", text: $height)
+                    .padding()
+                Button("Run"){
+                    result = triangleOfOnes(with: height)
+                }
+                
+                Text(result)
+            }
+        }
+    }
+    
+    // TODO: Complete the function so that it returns the correct triangle of ones based on the provided height
+    func triangleOfOnes(with height: String) -> String {
+        guard let height = Int(height), height > 0 else { return invalidInputText}
+        
+        if height == 1 {
+            return "1"
+        }
+        
+        var output = ""
+        let numberOfRows = (height * 2) - 1
+        
+        for row in 1...numberOfRows {
+            let numberOfOnes = row <= height ? row : (numberOfRows - row) + 1
+            output.append(String(repeating: "1", count: numberOfOnes))
+            
+            if row < numberOfRows {
+                output.append("\n")
+            }
+        }
+        
+        return output
+    }    
+}
+
+struct PyramidOfOnesInstructions_Previews: PreviewProvider {
+    static var previews: some View {
+        DynamicTriangleView()
+    }
+}
