355dddb37f0cb6a7e98d54e00bd1c93e5db2681b
TylerLawrence <tyler.lawrence15@gmail.com>
2022-02-03 15:39:26 -0500

    now providing example cases with randomly generated strings

--- KataClub/Logic/SubstringView.swift
+++ KataClub/Logic/SubstringView.swift
@@ -19,18 +19,59 @@ struct SubstringView: View {
     
     """
     
+    @State private var inputStringExample: String = ""
+    @State private var inputSubstringExample: String = ""
+    
+    @State private var output: Int = 0
+    
     var body: some View {
         VStack{
             Text("Substrings")
                 .font(.largeTitle)
             Text(instructions)
                 .padding([.leading, .trailing])
+            Divider()
+            
+            Button("generate random input strings"){
+                inputSubstringExample = generateRandomInputs().0
+                inputStringExample = generateRandomInputs().1
+            }
+            //Text("string: \(inputStringExample)")
+            //Text("substring: \(inputSubstringExample)")
+            Divider()
+            
+            if !inputStringExample.isEmpty{
+                Text("count how many times '\(inputSubstringExample)' occurs in '\(inputStringExample)'")
+                Button("count occurrences of substring"){
+                    output = calculateOccurrences(inputString: inputStringExample, inputSubstring: inputSubstringExample)
+                }
+                Text("\(output)")
+            }
+            
         }
     }
     
     // TODO: Fill in the function to count how many times inputSubstring occurs within inputString
     func calculateOccurrences(inputString:String, inputSubstring:String) -> Int {
         return 0
+    }
+    
+    func generateRandomInputs() -> (String, String) {
+        
+        let letters = ["k", "o", "t"]
+        
+        var inputSubstring = ""
+        var inputString = ""
+        
+        for _ in 1...1{
+            inputSubstring+=letters.randomElement()!
+        }
+
+        for _ in 1...9{
+            inputString+=letters.randomElement()!
+        }
+        
+        return (inputSubstring, inputString)
     }
 }
 
